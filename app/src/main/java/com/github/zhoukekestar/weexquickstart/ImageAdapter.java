6bbf0f5606f13eace5af5169eb26223d00ae444d
keke <zhoukekestar@163.com>
2016-10-21 14:47:52 +0800

    Updated: image adapter

--- app/src/main/java/com/github/zhoukekestar/weexquickstart/ImageAdapter.java
+++ app/src/main/java/com/github/zhoukekestar/weexquickstart/ImageAdapter.java
@@ -1,7 +1,11 @@
 package com.github.zhoukekestar.weexquickstart;
 
+import android.text.TextUtils;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+import com.taobao.weex.WXEnvironment;
+import com.taobao.weex.WXSDKManager;
 import com.taobao.weex.adapter.IWXImgLoaderAdapter;
 import com.taobao.weex.common.WXImageStrategy;
 import com.taobao.weex.dom.WXImageQuality;
@@ -13,8 +17,31 @@ public class ImageAdapter implements IWXImgLoaderAdapter {
 
 
     @Override
-    public void setImage(String url, ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
+    public void setImage(final String url, final ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
+        WXSDKManager.getInstance().postOnUiThread(new Runnable() {
 
+            @Override
+            public void run() {
+                if(view==null||view.getLayoutParams()==null){
+                    return;
+                }
+                if (TextUtils.isEmpty(url)) {
+                    view.setImageBitmap(null);
+                    return;
+                }
+                String temp = url;
+                if (url.startsWith("//")) {
+                    temp = "http:" + url;
+                }
+                if (view.getLayoutParams().width <= 0 || view.getLayoutParams().height <= 0) {
+                    return;
+                }
+                Picasso.with(WXEnvironment.getApplication())
+                        .load(temp)
+                        .into(view);
+            }
+        },0);
         // Code comes form: https://github.com/weexteam/article/issues/25
+        // https://github.com/weexteam/article/issues/27
     }
 }
